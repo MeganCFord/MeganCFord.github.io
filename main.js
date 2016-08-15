677a4a830aa8596fb4368dc0ab927021b68c8032
Megan Ford <meg.c.ford@gmail.com>
2016-08-15 13:37:10 -0500

    added randomized splash images, edited body text font and line-height.

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 
 // enable bootstrap scrollspy.
-$('body').scrollspy({target: "#collapsing-nav-options"});
+$(document).ready(()=> {
+  
+  //randomize splash images.
+  const splashImages = ["asheville", "comicon", "dc", "giacometti", "ginormica", "moma", "panda", "picasso", "tomato"];
+  const thisImg = splashImages[Math.floor(Math.random() * splashImages.length)];
+  $(".splash").css({"background-image": `url(img/splash/${thisImg}.jpg)`});
+  
+  //enable bootstrap scrollspy.
+  $("body").scrollspy({target: "#collapsing-nav-options"});
 
-// enable bootstrap tooltip.
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-});
+  // enable bootstrap tooltip.
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
 
-
-$(document).scroll(function () {
+  //show navbar once secondary nav scrolls to it.
+  $(document).scroll(() => {
     const y = $(document).scrollTop();
     const t = $("#secondary-nav").offset().top;
     if (y > t) {
-        $(".navbar").fadeIn();
+      $(".navbar").fadeIn();
     } else {
-        $(".navbar").fadeOut();
+      $(".navbar").fadeOut();
     }
+  });
+
 });
